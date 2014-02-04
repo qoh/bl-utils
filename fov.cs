88db23d157b1a5a44bfd292b2ef8e86534d841cc
Port <portification@gmail.com>
2014-02-04 13:33:33 +0100

    Update fov.cs

--- fov.cs
+++ fov.cs
@@ -1,12 +1,36 @@
-function ShapeBase::isPointWithinFOV(%this, %point, %fov) {
-	if (%fov $= "") {
-		%fov = 90;
+// Public: Test if a point is within a view cone.
+//
+// a - The origin of the cone.
+// b - The point to test.
+// vector - The direction of the cone.
+// angle - The angle size of the cone (default: 90).
+//
+// Returns true or false.
+function isWithinView(%a, %b, %vector, %angle)
+{
+	if (%angle $= "")
+	{
+		%angle = 90;
 	}
 
-	%product = vectorDot(
-		%this.getEyeVector(),
-		vectorNormalize(vectorSub(%point, %this.getWorldBoxCenter()))
+	%product = vectorDot(%vector,
+		vectorNormalize(vectorSub(%b, %a))
 	);
 
-	return %product >= 1 - (%fov / 360) * 2;
+	return %product >= 1 - (%angle / 360) * 2;
+}
+
+function Player::isWithinView(%this, %point, %angle)
+{
+	if (%angle $= "")
+	{
+		%client = %this.getControllingClient();
+
+		if (isObject(%client))
+		{
+			%angle = %client.getControlCameraFOV();
+		}
+	}
+
+	return isWithinView(%this.getEyePoint(), %b, %this.getEyeVector(), %angle);
 }
