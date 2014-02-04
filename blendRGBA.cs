9cc15198a3e84342f01e1d24d4af268b89f93416
Port <portification@gmail.com>
2014-02-04 13:48:46 +0100

    Update blendRGBA.cs

--- blendRGBA.cs
+++ blendRGBA.cs
@@ -1,11 +1,24 @@
-function blendRGBA(%bg, %fg) {
+function blendRGBA(%bg, %fg)
+{
 	%ba = getWord(%bg, 3);
 	%fa = getWord(%fg, 3);
 
 	%a = 1 - (1 - %fa) * (1 - %ba);
+
 	%r = getWord(%fg, 0) * %fa / %a + getWord(%bg, 0) * %ba * (1 - %fa) / %a;
 	%g = getWord(%fg, 1) * %fa / %a + getWord(%bg, 1) * %ba * (1 - %fa) / %a;
 	%b = getWord(%fg, 2) * %fa / %a + getWord(%bg, 0) * %ba * (1 - %fa) / %a;
 
 	return %r SPC %g SPC %b SPC %a;
+}
+
+function desaturateRGB(%r, %g, %b, %k)
+{
+	%i = (%r + %g + %b) / 3;
+
+	%r = %i * %k + %r * (1 - %k);
+	%g = %i * %k + %g * (1 - %k);
+	%b = %i * %k + %b * (1 - %k);
+
+	return %r SPC %g SPC %b;
 }
